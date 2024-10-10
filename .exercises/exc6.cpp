eff316f90037e16b3e770c7f8b3afecaadf2a1d3
tootiltedboy <belinsky_d@hotmail.com>
2024-10-10 14:10:35 +0300

    Made a 6th task about password verification programme

---
+++ .exercises/exc6.cpp
@@ -0,0 +1,43 @@
+/*Simple password verification programme. Each password must be from 8 to 14 symbols,
+and at least 3 out of 4 special symbol groups */
+#include <iostream>
+#include <string>
+bool Verification(const std::string& password) {
+    if (password.size() < 8 || password.size() > 14) {
+        return false;
+    }
+    int upper = 0;
+    int lower = 0;
+    int digit = 0;
+    int other = 0;
+
+    for (char c : password) {
+        if (c < 33 || c > 126) {
+            return false;
+        }
+        if ('A' <= c && c <= 'Z') {
+            upper = 1;
+        }
+        else if ('a' <= c && c<= 'z') {
+            lower = 1;
+        }
+        else if ('0' <= c && c<= '9') {
+            digit = 1;
+        }
+        else {
+            other = 1;
+        }
+    }
+    return upper+lower+digit+other >= 3;
+}
+int main() {
+    std::string password;
+    std::cout << "Please enter your password:\n";
+    std::getline(std::cin, password);
+    if (Verification(password)) {
+        std::cout << "Password verified and accepted" << std::endl;
+    }
+    else {
+        std::cout << "Wrong password\n" << "Please try again later" << std::endl;
+    }
+}
