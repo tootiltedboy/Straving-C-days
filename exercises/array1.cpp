cd6dc6ce4c7ccd35d9e032afc8a56f1e07e45d29
tootiltedboy <belinsky_d@hotmail.com>
2024-10-14 14:40:07 +0300

    Built the first array theme task about finding the maximum value of the created array

---
+++ exercises/array1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <thread>
+using namespace std;
+
+int main() {
+    int max_value;
+    bool variable;
+
+    cout << "Каким образом вы хотите инициализировать массив:" << endl;
+    this_thread::sleep_for(250ms);
+    cout << "1 - вручную" << endl;
+    this_thread::sleep_for(250ms);
+    cout << "0 - автоматически" << endl;
+    this_thread::sleep_for(250ms);
+    cout << "Введите выбранную опцию: ";
+    cin >> variable;
+
+    if (variable == true) {
+        int N;
+        cout << "Введите кол-во элементов массива: ";
+        cin >> N;
+
+        int arr[N];
+        for (int i = 0; i < N; i++) {
+            cout << "Введите " << i + 1 << "-й элемент массива: ";
+            cin >> arr[i];
+        }
+
+
+        max_value = arr[0];
+        for (int j = 1; j < N; j++) {
+            if (arr[j] > max_value) {
+                max_value = arr[j];
+            }
+        }
+    } else {
+        srand(static_cast<unsigned int>(time(nullptr)));
+        int r = rand() % 257 + 1;
+        unsigned int arr[r];
+
+        for (int i = 0; i < r; i++) {
+            arr[i] = rand() % 256;
+        }
+
+        max_value = arr[0];
+        for (int j = 1; j < r; j++) {
+            if (arr[j] > max_value) {
+                max_value = arr[j];
+            }
+        }
+    }
+    cout << "Наибольший элемент массива это: " << max_value << endl;
+    return 0;
+}
